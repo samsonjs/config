0dd0f214db9b289e2805bd55ee7c7d2c50c0695a
Sami Samhuri <sami.samhuri@gmail.com>
2010-08-18 22:04:58 -0700

    list symlinks directly, simple is better here

--- init.sh
+++ init.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
+cd
+
 BASENAME="${0##*/}"
 
 if [ x"$1" != x ]; then
     CONFIG_PATH="$1"
 else
-    CONFIG_PATH="$HOME/config"
+    CONFIG_PATH="${HOME}/config"
 fi
 
-for FILE in $CONFIG_PATH/*; do
-    DOTFILE=".${FILE##*/}"
-    if [ "$DOTFILE" = ".${BASENAME}" ]; then continue; fi
-    if [ -e "$DOTFILE" ]; then
-        mkdir original-dot-files >/dev/null 2>/dev/null
-        echo "Existing file found at $DOTFILE, moving to ~/original-dot-files."
-        mv "$DOTFILE" original-dot-files/
-    fi
-    ln -s "$FILE" "$DOTFILE"
-done
+ln -s "${CONFIG_PATH}/emacs"    ".emacs"
+ln -s "${CONFIG_PATH}/emacs.d"  ".emacs.d"
+ln -s "${CONFIG_PATH}/screenrc" ".screenrc"
+ln -s "${CONFIG_PATH}/vimrc"    ".vimrc"
+ln -s "${CONFIG_PATH}/zshenv"   ".zshenv"
