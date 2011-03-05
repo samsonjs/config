8a043d414ba0ed7def9669e8ffb2508576b48ddc
Sami Samhuri <sami.samhuri@gmail.com>
2011-03-04 19:16:49 -0800

    DRY up init.sh and add .gitconfig

--- init.sh
+++ init.sh
@@ -8,9 +8,13 @@ else
     CONFIG_PATH="${HOME}/config"
 fi
 
-[ -e ".ackrc"    ] || ln -s "${CONFIG_PATH}/ackrc"    ".ackrc"
-[ -e ".emacs"    ] || ln -s "${CONFIG_PATH}/emacs"    ".emacs"
-[ -e ".emacs.d"  ] || ln -s "${CONFIG_PATH}/emacs.d"  ".emacs.d"
-[ -e ".screenrc" ] || ln -s "${CONFIG_PATH}/screenrc" ".screenrc"
-[ -e ".vimrc"    ] || ln -s "${CONFIG_PATH}/vimrc"    ".vimrc"
-[ -e ".zshenv"   ] || ln -s "${CONFIG_PATH}/zshenv"   ".zshenv"
+link_config() {
+    file="$1"
+    [ -e ".$file" ] || ln -s "${CONFIG_PATH}/$file" ".$file"
+}
+
+CONFIG_FILES="ackrc emacs emacs.d gitconfig screenrc vimrc zshenv"
+
+for file in $CONFIG_FILES; do
+    link_config "$file"
+done
